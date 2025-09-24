7aa037bac2f9c4e52d90ed5bd34e43a768fbcbaa
Mikhail <mikhailcazi@gmail.com>
2025-09-24 02:15:08 +0200

    Switching to Render backend

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,8 +2,8 @@ import { FormSchema } from "@/lib/schema/formSchema";
 import axios from "axios";
 import * as z from "zod";
 
-// const API_BASE = "https://undp-backend.onrender.com";
-const API_BASE = "http://localhost:3000"; // or from env
+const API_BASE = "https://undp-backend.onrender.com";
+// const API_BASE = "http://localhost:3000"; // or from env
 
 export const api = {
   getProposals: () => axios.get(`${API_BASE}/proposals`),
